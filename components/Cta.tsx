b05a2de97f88857966822f00127314673d0e5a49
SegFault <cryptexunion@gmail.com>
2023-12-02 17:33:05 +0100

    DashboardNav split

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -17,20 +17,20 @@ interface CtaProps {
 
 const Cta: React.FC<CtaProps> = ({title, btnText, btnLink}) => (
   <Card className="bg-gradient-to-r from-zinc-900 to-black rounded-lg shadow-md mx-auto my-4 hover:border-blue-500 transition-colors delay-50">
-      <CardContent className="flex items-center justify-between p-6">
-        <CardTitle className="md:text-4xl font-bold tracking-tight sm:text-2xl lg:text-4xl max-w-xl text-white">
-          {title} 
-          {/* <span className="font-extrabold text-gray-300">Naga</span> */}
-        </CardTitle>
-        <div className="flex justify-end">
+    <CardContent className="flex flex-col sm:flex-row items-center justify-between p-6">
+      <CardTitle className="md:text-4xl font-bold tracking-tight sm:text-2xl lg:text-4xl max-w-xl text-white mb-4 sm:mb-0">
+        {title} 
+        {/* <span className="font-extrabold text-gray-300">Naga</span> */}
+      </CardTitle>
+      <div className="flex justify-end">
         <Link href={btnLink}>
           <Button className="bg-white text-gray-600 hover:bg-neutral-900 hover:text-white">
-          <MessagesSquare className="mr-2 h-4 w-4" /> {btnText}
+            <MessagesSquare className="mr-2 h-4 w-4" /> {btnText}
           </Button>
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+        </Link>
+      </div>
+    </CardContent>
+  </Card>
 );
 
 export default Cta;
--- components/dashboard/DashboardNav.tsx
+++ components/dashboard/DashboardNav.tsx
@@ -1,55 +1,73 @@
-
-
-import '@/app/globals.css'
-
-import Core from '@/conf/cfg'
-import { Brain } from 'lucide-react'
-
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import Link from 'next/link'
-import DashboardNav from '@/components/dashboard/DashboardNav'
-
-export default function DashboardLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
+import Core from "@/conf/cfg"
+import { Brain } from "lucide-react"
+import Link from "next/link"
+const DashboardNav = () => {
     return (
-      <div key="1" className="grid h-screen grid-cols-[260px_1fr] ">
-     <DashboardNav />
-      <div className="flex flex-col">
-        <header className="flex items-center h-16 px-4 border-b dark:border-neutral-00">
-          <form className="flex-1">
-            <div className="relative">
-              <IconSearch className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
-              <Input className="pl-8 w-full bg-white dark:bg-gray-950" placeholder="Search..." type="search" />
+        <nav className="border-r dark:border-neutral-800 bg-gray-100/40 dark:bg-neutral-900/20 h-full">
+        <div className="flex flex-col gap-2 h-full justify-between">
+          <div>
+            <div className="flex items-center h-16 px-4 border-b dark:border-neutral-800 justify-between">
+              <Link className="flex items-center gap-2 font-semibold" href="#">
+                <Brain className="h-6 w-6" />
+                <span>{Core.name}</span>
+              </Link>
+              <span className="text-gray-500 dark:text-gray-400">{Core.version}</span>
             </div>
-          </form>
-          <Button className="rounded-full ml-4" size="icon" variant="ghost">
-            <img
-              alt="Avatar"
-              className="rounded-full"
-              height="32"
-              src="/placeholder.svg"
-              style={{
-                aspectRatio: "32/32",
-                objectFit: "cover",
-              }}
-              width="32"
-            />
-            <span className="sr-only">User profile</span>
-          </Button>
-        </header>
-          <main className="flex flex-1 p-4 md:p-6 max-h-screen">
-           
-             {children}
-           
-          </main>
+            <div className="flex-1 px-4">
+              <h2 className="font-semibold text-lg mb-4">Navigation</h2>
+              <ul className="space-y-2">
+                <li>
+                  <Link
+                    className="flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                    href="#"
+                  >
+                    <IconDashboard className="h-4 w-4" />
+                    Models{"\n                                  "}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                    href="#"
+                  >
+                    <IconProjects className="h-4 w-4" />
+                    Limits{"\n                                  "}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                    href="#"
+                  >
+                    <IconTasks className="h-4 w-4" />
+                    Playground{"\n                                  "}
+                  </Link>
+                </li>
+                
+                
+              </ul>
+            </div>
+          </div>
+          {/* <div className="px-4 pb-4">
+            <div className="mt-8 flex flex-col items-center bg-gray-200 dark:bg-gray-800 rounded-lg p-4">
+              <h2 className="font-semibold text-lg mb-4 text-center">User Panel</h2>
+              <Avatar className="h-16 w-16 mb-2">
+                <AvatarImage alt="User Avatar" src="/placeholder-avatar.jpg" />
+                <AvatarFallback>UA</AvatarFallback>
+              </Avatar>
+              <div className="font-medium mb-2">sgflt</div>
+              <div className="text-sm text-green-500 dark:text-green-400 mb-2">Online</div>
+              <div className="flex justify-between w-full mt-2 flex-col md:flex-row text-center">
+                <div className="text-sm text-gray-500 dark:text-gray-400 mb-2 md:mb-0">Credits left</div>
+                <div className="text-sm text-gray-500 dark:text-gray-400">Refreshes on: 12/12/2023</div>
+              </div>
+            </div>
+          </div> */}
         </div>
-      </div>
+      </nav>
     )
-    }
+}
+export default DashboardNav
 function IconDashboard(props: React.SVGProps<SVGSVGElement>) {
     return (
       <svg
