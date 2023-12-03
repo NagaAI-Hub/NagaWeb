042ac3301914808e4c2cd00b5c59fc7507c6c584
SegFault <cryptexunion@gmail.com>
2023-12-03 03:28:41 +0100

    x

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,29 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
-import { Core } from '@/conf/cfg'
+const inter = Inter({ subsets: ['latin'] })
+import {Core} from '@/conf/cfg'
 import StoreProvider from './StoreProvider'
 import Head from 'next/head'
-import Script from 'next/script'
-
-const inter = Inter({ subsets: ['latin'] })
+export const metadata: Metadata = {
+  title: Core.title,
+  description: Core.description,
+  keywords: 'AI technology services, advanced AI solutions, AI for developers, AI for businesses, NagaAI, AI APIs, cutting-edge AI, bleeding-edge AI, gpts, chatgpt, gpt4, AI',
+  metadataBase: typeof process.env.NEXT_PUBLIC_WEB_URL === 'string' ? new URL(process.env.NEXT_PUBLIC_WEB_URL) : null,
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: process.env.NEXT_PUBLIC_WEB_URL,
+    siteName: Core.name,
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_WEB_URL}logos/naga.png`,
+        width: 600,
+        height: 600,
+      }
+    ]
+  }
+}
 interface Image {
   url: string;
   width: number;
@@ -29,26 +46,6 @@ interface Metadata {
   metadataBase: URL | null;
   openGraph: OpenGraph;
 }
-export const metadata: Metadata = {
-  title: Core.title,
-  description: Core.description,
-  keywords: 'AI technology services, advanced AI solutions, AI for developers, AI for businesses, NagaAI, AI APIs, cutting-edge AI, bleeding-edge AI, gpts, chatgpt, gpt4, AI',
-  metadataBase: typeof process.env.NEXT_PUBLIC_WEB_URL === 'string' ? new URL(process.env.NEXT_PUBLIC_WEB_URL) : null,
-  openGraph: {
-    type: 'website',
-    locale: 'en_US',
-    url: process.env.NEXT_PUBLIC_WEB_URL,
-    siteName: Core.name,
-    images: [
-      {
-        url: `${process.env.NEXT_PUBLIC_WEB_URL}logos/naga.png`,
-        width: 600,
-        height: 600,
-      }
-    ]
-  }
-}
-
 export default function RootLayout({
   children,
 }: {
@@ -56,7 +53,7 @@ export default function RootLayout({
 }) {
   
   return (
-    <>
+    <html lang="en" suppressHydrationWarning>
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
@@ -68,18 +65,21 @@ export default function RootLayout({
         {metadata.openGraph.images.map((image, index) => (
           <meta key={index} property="og:image" content={image.url} />
         ))}
+        
       </Head>
       <StoreProvider>
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
+          
           {children}
         </ThemeProvider>
-      </StoreProvider>
-      
-    </>
+      </body>
+          </StoreProvider>
+    </html>
   )
 }
