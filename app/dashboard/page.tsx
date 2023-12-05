f84d8d6a0e2c83f3a79465f79561040452ad46cc
SegFault <cryptexunion@gmail.com>
2023-12-05 20:27:02 +0100

    x

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,2 +1,16 @@
+import { Card } from "@/components/ui/card"
 
-import Dashboard from './Dashboard'
+export default async function Dashboard() {
+ 
+  return (
+    <>
+        <div 
+            className="grid gap-4 grid-cols-1 w-full h-screen overflow-auto">
+              <Card>
+              <iframe className="w-full h-full" src="https://chat.naga.ac/" />
+              </Card>
+            </div>
+
+    </>
+  )
+}
