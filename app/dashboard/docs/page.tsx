f631461e4e737c75319ad118425b477ec593bb1c
SegFault <cryptexunion@gmail.com>
2023-12-09 15:02:39 +0100

    refactor

--- app/dashboard/docs/page.tsx
+++ app/dashboard/docs/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
-import { Endpoints } from "@/conf/cfg";
 import { Terminal } from "lucide-react";
 {/* @ts-ignore */ }
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 {/* @ts-ignore */ }
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Endpoints } from "@/conf/cfg";
 import {
     Tabs,
     TabsContent,
@@ -15,38 +15,48 @@ import {
 
 import {pythonCodes, javascriptCodes} from "./codes";
 import Link from "next/link";
-const Docs = () => {
-    const OAI_PATH_VARS = ["OPENAI_API_KEY", "OPENAI_API_BASE"]
-    const R_Links = ["https://api.naga.ac/docs", "https://platform.openai.com/docs/introduction", "https://developer.mozilla.org/en-US/", "https://www.freecodecamp.org/news/python-fundamentals-for-data-science/"]
-    return (
-        <div>
-            <div className="grid grid-cols-2">
-                <div>
-                    <Alert>
-                        <Terminal className="h-4 w-4" />
-                        <AlertTitle>Attention required!</AlertTitle>
-                        <AlertDescription>
-                            Those docs are evolving and are not complete yet. Be warned that they might change, vary and be incomplete.
-                        </AlertDescription>
-                    </Alert>
-                    <h3 className="my-4 scroll-m-20 text-3xl font-semibold tracking-tight">
-                        Understanding the Flexibility of Systems with Naga API
-                    </h3>
-                    <p className="text-muted-foreground">The inherent beauty of systems lies in their volatility and adaptability. They offer a remarkable degree of maneuverability and freedom, enabling changes to be made seamlessly at any point.</p>
-                    <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
-                        Rapid Implementation with Naga API
-                    </h3>
-                    <p className="text-muted-foreground">When you engage with the Naga API, the goal is straightforward: to swiftly integrate our API into your applications or local tools. This process is designed for efficiency and ease of use.</p>
-                    <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
-                        Example Use Case: Running PR Agent Locally
-                    </h3>
-                    <p className="text-muted-foreground">Consider the scenario where you wish to run a local instance of PR Agent. The standard documentation indicates the necessity of an OpenAI API key. While this is accurate, the structure of these systems allows for a more direct approach. You can utilize the Naga API without modifying any existing code.</p>
-                    <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
-                        Leveraging Official OpenAI Packages
-                    </h3>
-                    <p className="text-muted-foreground">The official OpenAI packages available on PyPI utilize PATH variables such as </p>
+const OAI_PATH_VARS = ["OPENAI_API_KEY", "OPENAI_API_BASE"];
+const R_Links = [
+    "https://api.naga.ac/docs", 
+    "https://platform.openai.com/docs/introduction", 
+    "https://developer.mozilla.org/en-US/", 
+    "https://www.freecodecamp.org/news/python-fundamentals-for-data-science/"
+];
 
-                    <div className="my-4 bg-neutral-900 p-4 rounded-xl">
+const DocumentationAlert = () => (
+    <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Attention required!</AlertTitle>
+        <AlertDescription>
+            Those docs are evolving and are not complete yet. Be warned that they might change, vary and be incomplete.
+        </AlertDescription>
+    </Alert>
+);
+
+const DocumentationContent = () => (
+    <>
+        {/* Add various sections of the documentation content here */}
+        <ExampleUseCase />
+        <LeveragingOfficialPackages />
+        {/* ... other sections ... */}
+    </>
+);
+const ExampleUseCase = () => (
+    <>
+        <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
+            Example Use Case: Running PR Agent Locally
+        </h3>
+        <p className="text-muted-foreground">Consider the scenario where you wish to run a local instance of PR Agent. The standard documentation indicates the necessity of an OpenAI API key. While this is accurate, the structure of these systems allows for a more direct approach. You can utilize the Naga API without modifying any existing code.</p>
+   
+    </>
+);
+const LeveragingOfficialPackages = () => (
+    <>
+        <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
+            Leveraging Official OpenAI Packages
+        </h3>
+        <p className="text-muted-foreground">he official OpenAI packages available on PyPI utilize PATH variables such as </p>
+        <div className="my-4 bg-neutral-900 p-4 rounded-xl">
                         {OAI_PATH_VARS.map((v, i) => (
                             <div key={i}>
                                 <span className="text-rose-500">{v}</span>
@@ -74,65 +84,75 @@ const Docs = () => {
                     </div>
                     <p className="text-muted-foreground">This script will append the path variables to your shell startup file. You can then replace the values with your Naga API key and Naga API base URL.
                     </p>
-                    <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
-                        Example Codes & Implementations
-                    </h3>
+                    
 
-                    <p className="text-muted-foreground">
+    </>
+);
+
+const ResourceLinks = ({ links }) => (
+    <>
+    <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
+    Example Codes & Implementations
+    </h3>
+    <p className="text-muted-foreground">
                         On the right side of this webpage, you&apos;ll find sample code for Python and JavaScript. While similar in implementation, they differ in syntax, reflecting the architectural distinctions between the two languages. While not exhaustive, these examples serve as a solid starting point for using both OpenAI and NagaAI APIs.
                     </p>
                     <p className="text-muted-foreground py-2">
                         For other references, parameters, and endpoints please visit those resources:
 
                     </p>
-                    <ul className="text-muted-foreground">
-                        {R_Links.map((v, i) => (
-                            <li key={i}>
-                                <Link href={v}>
-                                    <p className="text-rose-500">{v}</p>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
+    <ul className="text-muted-foreground">
+        {links.map((link, i) => (
+            <li key={i}>
+                <Link href={link}>
+                    <p className="text-rose-500">{link}</p>
+                </Link>
+            </li>
+        ))}
+    </ul>
+    
+    </>
+);
 
+const CodeExamplesTabs = () => (
+    <Tabs defaultValue="python" className="">
+        <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="python">Python</TabsTrigger>
+            <TabsTrigger value="javascript">Javascript</TabsTrigger>
+        </TabsList>
+        <CodeExamples content={pythonCodes} language="python" />
+        <CodeExamples content={javascriptCodes} language="javascript" />
+    </Tabs>
+);
+const CodeExamples = ({ content, language }) => (
+    <TabsContent value={language}>
+        {Object.entries(content).map(([key, value]) => (
+            <div key={key}>
+                <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
+                    {value.title}
+                </h3>
+                <SyntaxHighlighter language={language} style={atomDark}>
+                    {value.code}
+                </SyntaxHighlighter>
+            </div>
+        ))}
+    </TabsContent>
+);
+const Docs = () => {
+    return (
+        <div>
+            <div className="grid grid-cols-2">
+                <div>
+                    <DocumentationAlert />
+                    <DocumentationContent />
+                    <ResourceLinks links={R_Links} />
                 </div>
                 <div className="px-4">
-                    <Tabs defaultValue="python" className="">
-                        <TabsList className="grid w-full grid-cols-2">
-                            <TabsTrigger value="python">Python</TabsTrigger>
-                            <TabsTrigger value="javascript">Javascript</TabsTrigger>
-                        </TabsList>
-                        <TabsContent value="python">
-                            {Object.entries(pythonCodes).map(([key, value]) => (
-                                <div key={key}>
-                                    <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
-                                        {value.title}
-                                    </h3>
-                                    <SyntaxHighlighter language="python" style={atomDark}>
-                                        {value.code}
-                                    </SyntaxHighlighter>
-                                </div>
-                            )
-                            )}
-
-                        </TabsContent>
-                        <TabsContent value="javascript">
-                        {Object.entries(javascriptCodes).map(([key, value]) => (
-                                <div key={key}>
-                                    <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">
-                                        {value.title}
-                                    </h3>
-                                    <SyntaxHighlighter language="python" style={atomDark}>
-                                        {value.code}
-                                    </SyntaxHighlighter>
-                                </div>
-                            )
-                            )}
-                        </TabsContent>
-                    </Tabs>
+                    <CodeExamplesTabs />
                 </div>
             </div>
         </div>
     );
-}
+};
+
 export default Docs
