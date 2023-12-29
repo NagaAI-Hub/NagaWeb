94343176c3fd2942f960f10bfedaa4d614884b2b
SegFault <cryptexunion@gmail.com>
2023-12-29 22:08:48 +0100

    Update function to handle edge cases
    This commit updates the function to handle edge cases, improving the overall reliability and robustness of the code.

--- components/PricingInfo.tsx
+++ components/PricingInfo.tsx
@@ -17,7 +17,7 @@ const PricingInfo: FC<PricingInfoProps> = ({ pricing }) => {
   } else if (pricing.per_image) {
     return <span> Image: {pricing.per_image}</span>;
   } else if (pricing.per_token) {
-    return <span> 1K Tokens I: {(pricing.per_token * 1000).toFixed(10)}</span>;
+    return <span> 1K Tokens In: {(pricing.per_token * 1000).toFixed(10)}</span>;
   } else if (pricing.per_second) {
     return <span> 1 Minute: {pricing.per_second * 60}</span>;
   }
