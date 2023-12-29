72eaf264ae3c85442fc0a81c4adc79c7d8dde632
SegFault <cryptexunion@gmail.com>
2023-12-29 22:07:52 +0100

    Refactor PricingInfo component to improve readability

--- components/PricingInfo.tsx
+++ components/PricingInfo.tsx
@@ -9,20 +9,20 @@ interface PricingInfoProps {
 const PricingInfo: FC<PricingInfoProps> = ({ pricing }) => {
   if (pricing.per_input_token && pricing.per_output_token) {
     return (
-      <div>
-        <span>1K Tokens I: {(pricing.per_input_token * 1000).toFixed(10)}</span>
-        <span>1K Tokens O: {(pricing.per_output_token * 1000).toFixed(10)}</span>
+        <div>
+        <span style={{ display: 'block' }}>1K Tokens In: {(pricing.per_input_token * 1000).toFixed(10)}</span>
+        <span style={{ display: 'block' }}>1K Tokens Out: {(pricing.per_output_token * 1000).toFixed(10)}</span>
       </div>
     );
   } else if (pricing.per_image) {
-    return <span>Image: {pricing.per_image}</span>;
+    return <span> Image: {pricing.per_image}</span>;
   } else if (pricing.per_token) {
-    return <span>1K Tokens I: {(pricing.per_token * 1000).toFixed(10)}</span>;
+    return <span> 1K Tokens I: {(pricing.per_token * 1000).toFixed(10)}</span>;
   } else if (pricing.per_second) {
-    return <span>1 Minute: {pricing.per_second * 60}</span>;
+    return <span> 1 Minute: {pricing.per_second * 60}</span>;
   }
   else if (pricing.per_character) {
-    return <span>1000 Characters: {pricing.per_character * 1000}</span>;
+    return <span> 1K Characters: {pricing.per_character * 1000}</span>;
   }
 
   return null;
