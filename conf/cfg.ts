2c3d9545f503189122ed4872694ba050136328a2
SegFault <cryptexunion@gmail.com>
2023-12-29 21:51:06 +0100

    Update service version to 3.6.5 for New Year

--- conf/cfg.ts
+++ conf/cfg.ts
@@ -2,7 +2,7 @@ export const Core = {
     // Service Name
     name: 'NagaAI',
     // Service Version
-    version: '3.6.0 Xmas',
+    version: '3.6.5 New Year',
     // Service Title
     title: 'NagaAI - Your Gateway to Cutting-Edge AI Technologies',
     // Service Description
