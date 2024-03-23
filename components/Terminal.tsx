4ef4d13ce328b7c10035dbadbc33ac54bad89100
segfault <cryptexunion@gmail.com>
2024-03-23 15:16:17 +0100

    aspect ratio

--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useRef, useState } from "react";
+import styled, { keyframes } from 'styled-components';
+
 const TerminalAnimation: React.FC = () => {
-    const terminalRef = useRef<HTMLDivElement>(null);
-    const [displayText, setDisplayText] = useState('');
-    const [isTyping, setIsTyping] = useState(true);
-    const [isAnimationVisible, setIsAnimationVisible] = useState(false);
-  
-    useEffect(() => {
-      const text = `
-  
-      ⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣶⣶⣿⣿⣿⣿⣿⣷⣶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⡿⠿⢿⣿⣶⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀
+  const terminalRef = useRef<HTMLDivElement>(null);
+  const [displayText, setDisplayText] = useState('');
+  const [isAnimationVisible, setIsAnimationVisible] = useState(false);
+
+  useEffect(() => {
+    const text = `
+      ⠀⢀⣠⣤⣶⣶⣿⣿⣿⣿⣿⣷⣶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⡿⠿⢿⣿⣶⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠞⠋⠉⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣷⣄⠀⠀⠀⠀⠀
 ⠀⠀⠀⣠⣾⣿⣿⣿⣿⠿⠛⠉⠁⠀⠀⠀⠀⠉⠙⠻⢿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣶⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣷⣄⠀⠀⠀
 ⠀⠀⣼⣿⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣷⡀⠀⠀⠀⢀⣶⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣧⠀⠀
@@ -23,49 +23,53 @@ const TerminalAnimation: React.FC = () => {
 ⠀⠀⠀⠈⠻⣿⣿⣿⣿⣶⣤⣀⣀⠀⠀⠀⣀⣀⣤⣶⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣶⣤⣀⣀⠀⠀⠀⢀⣀⣤⣶⣿⣿⣿⣿⠟⠁⠀⠀⠀
 ⠀⠀⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⠿⠿⠿⠿⠿⠟⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⢿⣿⣿⣿⠿⠿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
-      `
-      let index = 0;
-      const typingSpeed = 5;
-      const charsPerStep = 2;
-  
-      const typing = () => {
-        if (index < text.length) {
-          const endIndex = Math.min(index + charsPerStep, text.length);
-          const newText = text.slice(0, endIndex);
-          setDisplayText(newText);
-          index = endIndex;
-          setTimeout(typing, typingSpeed);
-        } else {
-          setIsTyping(false);
-          setTimeout(() => {
-            setIsAnimationVisible(true);
-          }, 500);
-        }
-      };
-  
-      setTimeout(typing, 1000);
-    }, []);
-  
-    return (
-      <div
-        ref={terminalRef}
-        className={`terminal bg-black text-white p-4 rounded-lg shadow-lg font-mono overflow-hidden ${
-          isAnimationVisible ? 'animate' : ''
-        }`}
-        style={{ width: '100%', height: '100%', fontSize: '16px' }}
-      >
-        <span className="text-green-500">
-          {isTyping ? (
-            <span className="animate-blink">█</span>
-          ) : (
-            <>
-              <span className="ml-1">{''}</span>
-            </>
-          )}
-        </span>{' '}
-        {displayText}
-      </div>
-    );
-  };
+`;
 
-  export default TerminalAnimation;
+    let index = 0;
+    const typingSpeed = 5;
+    const charsPerStep = 2;
+
+    const typing = () => {
+      if (index < text.length) {
+        const endIndex = Math.min(index + charsPerStep, text.length);
+        const newText = text.slice(0, endIndex);
+        setDisplayText(newText);
+        index = endIndex;
+        setTimeout(typing, typingSpeed);
+      } else {
+        setTimeout(() => {
+          setIsAnimationVisible(true);
+        }, 500);
+      }
+    };
+
+    setTimeout(typing, 1000);
+  }, []);
+
+  return (
+
+  <div
+ref={terminalRef}
+className={`terminal text-white p-4 rounded-lg font-mono overflow-hidden ${         isAnimationVisible ? 'animate' : ''       }}
+    }`}style={{ width: '100%', height: '100%' }}
+>
+<ResponsiveFont>{displayText}</ResponsiveFont>
+</div>
+  )
+};
+
+const ResponsiveFont = styled.pre`
+  
+  @media (max-width: 768px) {
+    font-size: 6px !important;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 8px !important;
+  }
+  @media (max-width: 375px) {
+    font-size: 6px !important;
+  }
+`;
+
+export default TerminalAnimation;
